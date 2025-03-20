95650646c4daa575a11fbbd08b57b58bd7c19757
Psicoses <demat0@proton.me>
2025-03-20 11:11:24 -0400

    Update StringHashMap.java

--- Ch.5_6/Maps/src/StringHashMap.java
+++ Ch.5_6/Maps/src/StringHashMap.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Random;
 
 public class StringHashMap implements StringMap{
     //Entry for every hash value
@@ -10,7 +11,13 @@ public class StringHashMap implements StringMap{
     // get value for corresponding key
     public String get(String k) {
         // 1. Get the right bucket using key's hashcode
-
+        int h = Math.abs(k.hashCode());
+        LinkedList<Entry> bucket = buckets[h % buckets.length];
+        for(Entry e : bucket){
+            if(e.key.equals(k)){
+                return e.value;
+            }
+        }
         // 2. Loop through the bucket looking for entry with matching key
 
         //3. Return value for matching entry
@@ -25,7 +32,7 @@ public class StringHashMap implements StringMap{
 
     public void put(String k, String v) {
         if(this.contains(k)) {
-            System.out.println("WARNING: " + s + " already in set");
+            System.out.println("WARNING: " + k + " already in set");
             return; // exit method
         }
 
@@ -40,5 +47,28 @@ public class StringHashMap implements StringMap{
 
     public int size() {
         return size;
+    }
+
+    public String toString() {
+        String all = "Map{";
+        for(int i = 0; i < buckets.length; i++) {
+            all += buckets[i] + ", ";
+        }
+        if(size > 0)
+            all = all.substring(0, all.length()-2);
+        return all + "}";
+    }
+
+    public static void main(String[] args){
+        String[] names = {"James", "Mike", "Rob", "John", "David", "Will", "richard", "Joe", "Tom", "Mary", "Pat", "Jenn", "Linda", "Liz", "Barb", "Susan", "Jessica"};
+
+        StringMap myMap = new StringHashMap();
+        for(int i = 0; i < 40; i++){
+            String randomName = names[i % names.length];
+
+            String randomAddress = i + " Sesame St.";
+            myMap.put(randomName, randomAddress);
+        }
+        System.out.println(myMap);
     }
 }
